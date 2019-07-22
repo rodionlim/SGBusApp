bd966395bf78c25cbd8a9a93bd12d511dc198c85
Rodion <rodion.lim@hotmail.com>
2019-07-22 22:36:47 +0800

    Second commit - Add all files

---
+++ app.py
@@ -0,0 +1,236 @@
+# Web Application backend Controller - SG Bus Timings
+"""
+Author: Rodion
+Date Created: 2019.07.14
+Version: 0.1
+ 
+"""
+from flask import Flask, jsonify, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from mysql.connector import MySQLConnection, Error
+import json
+from werkzeug.exceptions import default_exceptions, HTTPException, \
+InternalServerError
+from werkzeug.security import generate_password_hash, check_password_hash
+from helpers import insert_user, selectMaxUser, validate_user, apology, \
+read_db_config, query_view, create_view, delete_view, insert_view, \
+parse_view, login_required
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+
+# Ensure responses aren't cached
+#@app.after_request
+#def after_request(response):
+#    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+#    response.headers["Expires"] = 0
+#    response.headers["Pragma"] = "no-cache"
+#    return response
+
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    """Show Index Page"""
+    return render_template("index.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+     """Log user in"""
+
+     # Forget any user_id
+     session.clear()
+
+     # User reached route via POST (as by submitting a form via POST)
+     if request.method == "POST":
+
+         # Ensure username was submitted
+         if not request.form.get("username"):
+             return apology("must provide username", 400)
+
+         # Ensure password was submitted
+         elif not request.form.get("password"):
+             return apology("must provide password", 400)
+
+         # Query database for username
+         try:
+             conn = MySQLConnection(**read_db_config())
+             cursor = conn.cursor()
+             cursor.execute("SELECT * FROM USER WHERE USERNAME = %s", \
+                            (request.form.get("username"),))
+             rows = cursor.fetchall()
+         except Error as e:
+             print(e)
+             rows = "ERROR"
+         finally:
+             cursor.close()
+             conn.close()
+             if rows == "ERROR":
+                 return apology("Unexpected error.", 400)
+         
+         # Ensure username exists and password is correct
+         if len(rows) != 1 or not check_password_hash(rows[0][2], \
+               request.form.get("password")):
+             return apology("invalid username and/or password", 400)
+
+         # Remember which user has logged in
+         session["user_id"] = rows[0][0]
+
+         # Redirect user to home page
+         return redirect("/")
+
+     # User reached route via GET (as by clicking a link or via redirect)
+     else:
+         return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+     """Log user out"""
+
+     # Forget any user_id
+     session.clear()
+
+     # Redirect user to login form
+     return redirect("/")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+     """Register user"""
+
+     # User reach route via POST (as by submitting a form via POST)
+     if request.method == "POST":
+         # Retrieve key inputs from form
+         user = request.form.get("username")
+         password = request.form.get("password")
+         confirmation = request.form.get("confirmation")
+
+         # Begin validation chks
+         if not user:
+             return apology("must provide a username", 400)
+         elif not (password and confirmation):
+             return apology("must provide a password", 400)
+         elif password != confirmation:
+             return apology("passwords do not match", 400)
+         else:
+             result = insert_user(user, generate_password_hash(password))
+             if not result:
+                 return apology("username already taken", 400)
+             else:
+                 # Remember which user has logged in
+                 uid = selectMaxUser()
+                 session["user_id"] = uid
+
+                 # Redirect user to home page
+                 return redirect("/")
+     else:
+         return render_template("/register.html")
+
+
+@app.route("/editViews", methods=["GET", "POST"])
+@login_required
+def editViews():
+    """Show Edit View Page"""    
+    # User reach route via GET
+    if request.method == "GET":
+        data = query_view(session["user_id"],True)
+        views = list(set([x['VIEW'] for x in data]))
+        return render_template("editViews.html", views=json.dumps(views))
+
+
+@app.route("/check", methods=["GET"])
+@login_required
+def check():
+    """Return true if username available, else false, in JSON format"""
+    if validate_user(request.args.get("username")):
+        return jsonify(True)
+    else:
+        return jsonify(False)
+    
+    
+@app.route("/create", methods=["GET"])
+@login_required
+def createView():
+    """Return true if view was created, else false, in JSON format"""
+    if create_view(session["user_id"], request.args.get("viewname")):
+        return jsonify(True)
+    else:
+        return jsonify(False)
+        
+        
+@app.route("/deleteView", methods=["GET"])
+@login_required
+def deleteView():
+    """Return true if view was deleted, else false, in JSON format"""
+    viewID = request.args.get("viewid") if request.args.get("viewid") else False
+    if delete_view(session["user_id"], request.args.get("viewname"), viewID):
+        return jsonify(True)
+    else:
+        return jsonify(False)
+        
+        
+@app.route("/queryView", methods=["GET"])
+@login_required
+def queryView():
+    """Return true if view was deleted, else false, in JSON format"""
+    res = query_view(session["user_id"], view=request.args.get("viewname"))
+    if res:
+        return jsonify(res)
+    else:
+        return jsonify(False)
+    
+    
+@app.route("/insertView", methods=["GET"])
+@login_required
+def insertView():
+    """Return true if row was inserted in view, else false, in JSON format"""
+    res = insert_view(session["user_id"], view=request.args.get("viewname"), \
+                      viewbusstopservice=request.args.get("bssn"))
+    if res == 2:
+        return jsonify("2")
+    elif res == 1:
+        return jsonify(True)
+    else:
+        return jsonify(False)
+    
+
+@app.route("/getViews", methods=["GET"])
+@login_required
+def getViews():
+    """Generates the html for all the views a user has created"""
+    allViews = query_view(session["user_id"])
+    uniqueViews = query_view(session["user_id"],True)
+    return render_template("/getViews.html", av=allViews, uv=uniqueViews)
+
+
+@app.route("/queryAPI", methods=["GET"])
+@login_required
+def queryAPI():
+    """Queries the LTA API to populate views"""
+    view = request.args.get("viewname")
+    return jsonify(parse_view(session["user_id"], view))
+
+
+def errorhandler(e):
+     """Handle error"""
+     if not isinstance(e, HTTPException):
+         e = InternalServerError()
+     return apology(e.name, e.code)
+
+
+# Listen for errors
+for code in default_exceptions:
+     app.errorhandler(code)(errorhandler)
---
+++ helpers.py
@@ -0,0 +1,305 @@
+# Helper functions for SG Bus Timings Web Application
+
+import requests
+# import mysql.connector
+from flask import render_template, session, redirect
+from mysql.connector import MySQLConnection, Error
+from configparser import ConfigParser
+import datetime
+from functools import wraps
+""" mySQL password in pythonanywhere: password250691 """
+
+
+def login_required(f):
+    """
+    Decorate routes to require login.
+    http://flask.pocoo.org/docs/1.0/patterns/viewdecorators/
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
+ 
+
+def read_db_config(filename='config.ini', section='mysql'):
+    """ Read database configuration file and return a dictionary object
+    :param filename: name of the configuration file
+    :param section: section of database configuration
+    :return: a dictionary of database parameters
+    """
+# create parser and read ini configuration file
+    parser = ConfigParser()
+    parser.read(filename)
+ 
+# get section, default to mysql
+    db = {}
+    if parser.has_section(section):
+        items = parser.items(section)
+        for item in items:
+            db[item[0]] = item[1]
+    else:
+        raise Exception('{0} not found in the {1} file'.format(section, filename))
+ 
+    return db
+
+
+# Query USERVIEWS table, filtering for ID
+def query_view(ID, uniqueFlag=False, view=False):
+    try:
+        conn = MySQLConnection(**read_db_config())
+        cursor = conn.cursor(dictionary=True)
+        if not uniqueFlag:
+            if view:
+                query = "SELECT * FROM USERVIEWS WHERE ID = %s AND VIEW = %s"
+            else:
+                query = "SELECT * FROM USERVIEWS WHERE ID = %s"
+        else:
+            query = "SELECT * FROM UNIQUEVIEW WHERE ID = %s"
+        
+        if view:
+            cursor.execute(query, (ID, view)) 
+        else:
+            cursor.execute(query, (ID,))            
+        rows = cursor.fetchall()        
+        print("Total Rows: ", cursor.rowcount)
+        res = rows    
+    except Error as e:
+        print(e)   
+        res = None
+    finally:
+        cursor.close()
+        conn.close()
+        return res
+
+
+# Helper function to query all rows from USERVIEWS table
+def query_all():
+    try:
+        conn = MySQLConnection(**read_db_config())
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM USERVIEWS")
+        rows = cursor.fetchall()        
+        print("Total Rows: ", cursor.rowcount)
+        return rows    
+    except Error as e:
+        print(e)        
+    finally:
+        cursor.close()
+        conn.close()
+        
+
+# Validate if USERNAME is valid
+def validate_user(username):
+    """Return true if username available, else false, in JSON format"""
+    try:
+        conn = MySQLConnection(**read_db_config())
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM USER WHERE USERNAME = %s", (username,))
+        rows = cursor.fetchall()        
+        res = False if len(rows) == 1 else True
+    except Error as e:
+        print(e)
+        res = False
+    finally:
+        cursor.close()
+        conn.close()
+        return res
+        
+# Insert into USER TABLE when a user registers for an account
+def insert_user(username, pwd):
+    
+    # Validation for valid usernames
+    if not validate_user(username):
+        return         
+    
+    # Insertion SQL
+    query = "INSERT INTO USER(USERNAME,HASH) VALUES(%s,%s)"
+    args = (username, pwd)
+    
+    try:
+        conn = MySQLConnection(**read_db_config())
+        cursor = conn.cursor()
+        cursor.execute(query, args)        
+        if cursor.lastrowid:
+            print('last insert id', cursor.lastrowid)
+        else:
+            print('last insert id not found')            
+        conn.commit()    
+    except Error as e:
+        print(e)        
+    finally:
+        cursor.close()
+        conn.close()
+        return 1
+    
+    
+# Select max ID from USER table to store into session
+def selectMaxUser():
+    try:
+        conn = MySQLConnection(**read_db_config())
+        cursor = conn.cursor()
+        cursor.execute("select max(ID) from user")     
+        ID = cursor.fetchall()[0][0]
+    except Error as e:
+        print(e)
+        ID = None
+    finally:
+        cursor.close()
+        conn.close()
+        return ID
+    
+
+# Create View in UNIQUEVIEW
+def create_view(ID, view):
+    query = "INSERT INTO UNIQUEVIEW(VIEW,ID,IDVIEW) VALUES(%s,%s,%s)"
+    args = (view, ID, str(ID) + view)
+    
+    try:
+        conn = MySQLConnection(**read_db_config())
+        cursor = conn.cursor()
+        cursor.execute(query, args)     
+        conn.commit()
+        print("Created a new view")
+        res = 1
+    except Error as e:
+        print(e)
+        res = None        
+    finally:
+        cursor.close()
+        conn.close()
+        return res
+
+# Insert into USERVIEWS TABLE the View for a user
+# Param: ID: 1
+# Param: view: "home"     
+# Param: viewbusstopservice: "50189|145"
+def insert_view(ID, view, viewbusstopservice):
+    query = "INSERT INTO USERVIEWS(ID,VIEW,VIEWBUSSTOPSERVICE) VALUES(%s,%s,%s)"
+    args = (ID, view, viewbusstopservice)
+    
+    # Validation to check service exist in bus stop number
+    bs = int(viewbusstopservice.split('|')[0])
+    service = viewbusstopservice.split('|')[1]
+    data = queryAPI('ltaodataservice/BusArrivalv2?',
+                    { 'BusStopCode': bs }
+                   )
+    if not service in [x['ServiceNo'] for x in data['Services']]:
+        print('Service does not exist in Bus Stop')
+        return 2
+    
+    try:
+        conn = MySQLConnection(**read_db_config())
+        cursor = conn.cursor()
+        cursor.execute(query, args)                 
+        conn.commit()    
+        print("Inserted a new view")
+        res = 1
+    except Error as e:
+        print(e)
+        res = None      
+    finally:
+        cursor.close()
+        conn.close()
+        return res
+        
+# Delete a particular view from USERVIEWS and UNIQUEVIEW table
+def delete_view(ID, view, viewID=False):
+    query = "DELETE FROM USERVIEWS WHERE ID = %s AND VIEW = %s"
+    query2 = "DELETE FROM UNIQUEVIEW WHERE ID = %s AND VIEW = %s"
+    query3 = "DELETE FROM USERVIEWS WHERE ID = %s AND VIEW = %s and VIEWID = %s"
+    
+    try:
+        conn = MySQLConnection(**read_db_config())        
+        cursor = conn.cursor()
+        if viewID:
+            args = (ID, view, viewID)
+            cursor.execute(query3, args)
+        else:
+            args = (ID, view)
+            cursor.execute(query, args)
+            cursor.execute(query2, args)
+        conn.commit()        
+        print("Successful Deletion")
+        res = 1
+    except Error as e:
+        print(e)
+        res = None
+    finally:
+        cursor.close()
+        conn.close()
+        return res
+        
+        
+# Remove data from any table in the database - Use with caution 
+def reset_table(tab):
+    query = "TRUNCATE TABLE %s"
+    args = (tab, )
+    
+    try:
+        conn = MySQLConnection(**read_db_config())
+        cursor = conn.cursor()
+        cursor.execute(query, args)
+        conn.commit()        
+        print("Successful Deletion")
+    except Error as e:
+        print(e)
+    finally:
+        cursor.close()
+        conn.close()        
+
+
+# Authentication params
+headers = { 'AccountKey' : 'ifYZ8D/5SOy9X87JSPq1YQ==',
+            'accept' : 'application/json'}
+
+
+# API call 
+def queryAPI(path, params):
+    headers = eval(read_db_config(section='LTAAPI')['headers'])
+    uri = read_db_config(section='LTAAPI')['uri']
+    return requests.get(uri + path, headers=headers, params=params).json()
+
+
+# Get the bus arrival timings based on User ID and requested view
+def parse_view(ID, view):  
+    res = query_view(ID)
+    res_filtered = [x["VIEWBUSSTOPSERVICE"] for x in res if x["VIEW"] == view]
+    res_api = [queryAPI('ltaodataservice/BusArrivalv2?', \
+                        { 'BusStopCode' : x.split("|")[0], \
+                         'ServiceNo' : x.split("|")[1]}) for x in res_filtered]
+    
+    def fmtTime(ts):
+      if len(ts) != 25:
+          return "NA"
+      
+      diff = str(datetime.datetime.strptime(ts[:19],"%Y-%m-%dT%H:%M:%S") - \
+                 datetime.datetime.now())[:7]
+      if diff == "-1 day,":
+          return "Arrived"
+      else:
+          return diff
+    
+    def getBusTimings(data):
+        if data['Services'] == []:
+            return ["Service has ended"]
+        else:
+            return [fmtTime(data['Services'][0]['NextBus'+ ('' if x == 0 else \
+                     str(x + 1))]['EstimatedArrival']) for x in range(3)]
+    
+    return dict(zip(res_filtered,[getBusTimings(x) for x in res_api]))
+
+
+def apology(message, code=400):
+    """Render message as an apology to user."""
+    def escape(s):
+        """
+        Escape special characters.
+        https://github.com/jacebrowning/memegen#special-characters
+        """
+        for old, new in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"),
+                         ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
+            s = s.replace(old, new)
+        return s
+    return render_template("apology.html", top=code, bottom=escape(message)), code
